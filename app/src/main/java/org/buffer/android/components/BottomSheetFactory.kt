e177ba312f23c8842c5fb3d56dca94a17798b675
Joe Birch <hitherejoebirch@googlemail.com>
2019-06-28 13:57:45 +0100

    Pass color reference

--- app/src/main/java/org/buffer/android/components/BottomSheetFactory.kt
+++ app/src/main/java/org/buffer/android/components/BottomSheetFactory.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
+import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.view_item_selection_message_sheet.view.list_item_selection_sheet_options
 import kotlinx.android.synthetic.main.view_item_selection_message_sheet.view.text_item_selection_message
@@ -16,17 +18,19 @@ object BottomSheetFactory {
 
     fun createTextItemSelectionBottomSheet(
             context: Context,
+            @ColorRes textColor: Int,
             @StringRes titleResource: Int? = null,
             adapter: ArrayAdapter<*>,
             listener: AdapterView.OnItemClickListener,
             footerView: View? = null
     ): BottomSheetDialog {
-        return createTextItemSelectionBottomSheet(context, titleResource, messageResource = null,
+        return createTextItemSelectionBottomSheet(context, textColor, titleResource, messageResource = null,
                 adapter = adapter, listener = listener, footerView = footerView)
     }
 
     fun createTextItemSelectionBottomSheet(
             context: Context,
+            @ColorRes textColor: Int,
             @StringRes titleResource: Int? = null,
             @StringRes messageResource: Int? = null,
             adapter: ArrayAdapter<*>,
@@ -36,12 +40,13 @@ object BottomSheetFactory {
         val title = titleResource?.let { context.getString(titleResource) } ?: kotlin.run { null }
         val message = messageResource?.let { context.getString(messageResource) }
                 ?: kotlin.run { null }
-        return createTextItemSelectionBottomSheet(context, title, message, adapter, listener,
-                footerView)
+        return createTextItemSelectionBottomSheet(context, textColor, title, message, adapter,
+                listener, footerView)
     }
 
     fun createTextItemSelectionBottomSheet(
             context: Context,
+            @ColorRes textColor: Int,
             title: String? = null,
             message: String? = null,
             adapter: ArrayAdapter<*>,
@@ -53,11 +58,15 @@ object BottomSheetFactory {
                     as LinearLayout
             title?.let {
                 view.text_item_selection_title.text = title
+                view.text_item_selection_title.setTextColor(
+                        ContextCompat.getColor(context, textColor))
             } ?: run {
                 view.text_item_selection_title.visibility = View.GONE
             }
             message?.let {
                 view.text_item_selection_message.text = message
+                view.text_item_selection_message.setTextColor(
+                        ContextCompat.getColor(context, textColor))
             } ?: run {
                 view.text_item_selection_message.visibility = View.GONE
             }
